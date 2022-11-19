2cf9c1de177b800468ad26866af5061927f9a653
IreneLee18 <cc86418520@gmail.com>
2022-11-19 19:08:33 +0800

    update loading

--- src/page/Welcome/Welcome.js
+++ src/page/Welcome/Welcome.js
@@ -1,11 +1,13 @@
 import Loading from "../../components/Loading/Loading";
+import { useState, useEffect } from "react";
 function Welcome() {
-  return (
-    <>
-      <Loading />
-      <div>Welcome</div>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, [1000]);
+  }, []);
+  return <>{isLoading ? <Loading /> : <div>Welcome</div>}</>;
 }
 
 export default Welcome;
