a0a004639cdbf18c9178b174fe79bae4f6615028
IreneLee18 <cc86418520@gmail.com>
2022-11-23 15:24:11 +0800

    update retro image , css , data

--- src/page/Retro/Retro.js
+++ src/page/Retro/Retro.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import Description from "../../components/Description/Description";
+import BacklogFooter from "../../components/Backlog/BacklogFooter";
+import Modal from "../../components/Modal/Modal";
+import RD from "../../style/image/character/RD.png";
+import {
+  retro_description,
+  retros,
+  retro_correct_ID,
+} from "../../utils/data/RetroData";
+import useModal from "../../hooks/UseModal";
+import { useState, useRef } from "react";
 
 function Retro() {
+  const [done, setDone] = useState([]);
+  const modalRef = useRef();
+  const { modalText, modalBtnText, handleClickFinish } = useModal(
+    modalRef,
+    done,
+    retro_correct_ID
+  );
   return (
-    <div>Retro</div>
-  )
+    <>
+      <div className="container view90_center_mb48 retro">
+        <Description character={retro_description} />
+        <div className="retro_body">
+          {retros.map((retro) => (
+            <div className="retro_item" key={retro.id}>
+              <h2 className="retro_title">{retro.title}</h2>
+              <ul className="retro_content">
+                {retro.retro.map((item) => (
+                  <li key={item.id}>
+                    <span></span>
+                    <h3>{item.content}</h3>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <BacklogFooter
+          last={"/sprint_list"}
+          btnTxt={"我想我了解了！"}
+          handleClickOpenModal={handleClickFinish}
+        />
+      </div>
+      <Modal
+        ref={modalRef}
+        character={RD}
+        text={modalText}
+        btn_text={modalBtnText}
+      />
+    </>
+  );
 }
 
-export default Retro
+export default Retro;
---
+++ src/utils/data/RetroData.js
@@ -0,0 +1,44 @@
+export const retro_description = {
+  id: "rd",
+  title: "",
+  contents: [
+    [
+      "哇新來的，你真的很幸運，今天剛好是開發 B 組的 Retro，你也來見識一下，看看 Retro 都該做些什麼吧～～",
+      "我們會在會議裡請團隊成員提出哪些是做得好的地方、哪些可以繼續改善的地方？並記錄在 Confluence 中。",
+    ],
+    ["重點在於『正面表述』，你也思考看看，哪一些是適合 Retro 的回饋吧～～"],
+  ],
+};
+
+export const retros = [
+  {
+    title: "做得好的地方",
+    id:'left',
+    retro: [
+      {
+        id: "help",
+        content: "這次我幫了很多人救火耶。",
+      },
+      {
+        id: "cover",
+        content: "大家在開發上都會互相 cover，讓任務準時在時間內完成。",
+      },
+    ],
+  },
+  {
+    title: "有哪些可以做得更好？",
+    id:'right',
+    retro: [
+      {
+        id: "record",
+        content: "可以記錄這次的開發時間，讓預估團隊點數可以更精準。",
+      },
+      {
+        id: "delay",
+        content: "開發時間預估不準確，請後端下次改進，避免 delay 到我。",
+      },
+    ],
+  },
+];
+
+export const retro_correct_ID = "cover_record";
